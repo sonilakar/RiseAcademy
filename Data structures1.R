7a2463beb4c64dcca6e38d5297a2bb7d3ce1614a
Sonila Kar <skar4@asu.edu>
2020-07-04 16:19:26 -0700

    Initial commit

---
+++ Data structures1.R
@@ -0,0 +1,47 @@
+#-------------------DATA STRUCTURES IN R-----------------------
+
+#~~~~~~~~~~~~~~~SINGLE-MODE DATA STRUCTURES~~~~~~~~~~~~~~~~~~~~
+
+# *****   Defining a Vector *****
+
+numVector <- c(1, 2, 3, 4, 5)               #all numeric
+numVector
+
+charVector <- c("a", "b", "c", "d", "e")    #all characters
+charVector
+
+nameVector <- c(Female = 100, Male = 120)   #values have names
+nameVector
+
+#mode
+
+mode(numVector)
+
+#length
+
+length(charVector)
+
+#names
+
+names(nameVector)
+
+#   Defining a Array
+
+
+
+#   Defining a Matrix
+
+testMatrix <- matrix(1:12, nrow = 3, ncol =4)
+
+#MULTI-MODE DATA STRUCTURES
+
+#   Defining a List
+
+numVector <- c(1, 2, 3, 4, 5) 
+
+charVector <- c("a", "b", "c", "d", "e")
+
+nameVector <- c(Female = 100, Male = 120)
+
+
+#   Defining a Dataframe
