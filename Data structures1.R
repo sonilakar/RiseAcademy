93ee1ac15ecb8cd76f7189fe2af656d1191e5739
Sonila Kar <skar4@asu.edu>
2020-07-05 00:08:10 -0700

    Done

--- Data structures1.R
+++ Data structures1.R
@@ -13,6 +13,18 @@ charVector
 nameVector <- c(Female = 100, Male = 120)   #values have names
 nameVector
 
+#   Defining a Matrix
+
+testMatrix <- matrix(1:12, nrow = 3, ncol =4)
+
+#   Defining a Array
+
+testVector <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+testArray <- array(testVector, dim = c(3,4))
+testArray
+
+#Attributes
+
 #mode
 
 mode(numVector)
@@ -25,23 +37,28 @@ length(charVector)
 
 names(nameVector)
 
-#   Defining a Array
-
-
-
-#   Defining a Matrix
-
-testMatrix <- matrix(1:12, nrow = 3, ncol =4)
-
 #MULTI-MODE DATA STRUCTURES
 
 #   Defining a List
 
-numVector <- c(1, 2, 3, 4, 5) 
+emptyList <- list()
 
-charVector <- c("a", "b", "c", "d", "e")
+aVector <- c("a", "b", "c")
+aMatrix <- matrix(1:6, nrow = 3, ncol =2)
 
-nameVector <- c(Female = 100, Male = 120)
-
+unnamedList <- list(aVector, aMatrix)
+unnamedList
 
 #   Defining a Dataframe
+
+Weather <- data.frame(
+  Day = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"),                  
+  Temp = c(50, 55, 60, 47, 53, 57, 61)
+)
+
+Weather
+
+#-------------------Finish-----------------------
+
+#~~~~~~~~~~~~~~~~~~~~~~ With love, Sonila <3 ----
+
