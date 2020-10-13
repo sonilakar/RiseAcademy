8df45df803ecf4b268a6a6b94998b184c74c984a
Sonila Kar <skar4@asurite.asu.edu>
2020-10-12 18:35:17 -0700

    string manipulation

--- strings1.R
+++ strings1.R
@@ -1,4 +1,5 @@
 install.packages("stringr")
+
 library(stringr)
 
 phrase <- "I love to work with strings"
@@ -7,34 +8,50 @@ Rise <- "   Rise Academy   "
 string1 <- "What is"
 string2 <- "your name ?"
 
+
+#length
+
+str_length(phrase)
+
 #count 
 
-
+str_count(cities)
 
 #pattern
 
-
+str_detect(phrase, "ot")
+str_detect(phrase, "ov")
 
 #trim
 
-
+str_trim(Rise)
 
 #concatenate
 
+str_c(string1, string2, sep = " ")
+str_c(string1, string2, sep = ".")
+str_c(string1, string2, sep = "")
 
 
 #order
 
-
+str_order(cities)
 
 #sort
 
-
+str_sort(cities)
 
 #replace
 
 
-
+str_replace(phrase, "o", "i")
+str_replace_all(phrase, "o", "i")
 
 #split
+
+str_split(phrase, "to")
+
+
+
+
 
