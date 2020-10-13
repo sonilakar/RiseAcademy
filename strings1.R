f422871ea4fc60573268cbe60028878b81017b51
Sonila Kar <skar4@asurite.asu.edu>
2020-10-12 17:55:54 -0700

    strings

---
+++ strings1.R
@@ -0,0 +1,40 @@
+install.packages("stringr")
+library(stringr)
+
+phrase <- "I love to work with strings"
+cities <- c("Chicago", "San Francisco", "Miami")
+Rise <- "   Rise Academy   "
+string1 <- "What is"
+string2 <- "your name ?"
+
+#count 
+
+
+
+#pattern
+
+
+
+#trim
+
+
+
+#concatenate
+
+
+
+#order
+
+
+
+#sort
+
+
+
+#replace
+
+
+
+
+#split
+
