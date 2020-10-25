d75488ecad584fff23582ea56bb7ac0806343b5e
Sonila Kar <skar4@asurite.asu.edu>
2020-10-24 20:31:50 -0700

    commit

--- grouping data.R
+++ grouping data.R
@@ -1,30 +1,31 @@
-#Group by
+#HOW TO TO GROUP BY IN R
 
 property_data <- read.csv("propertydata.csv",
                             stringsAsFactors = FALSE)
 
-###aggregate function
+#AGGREGATE
 
 d <- aggregate(PRICE ~ PROPERTYCITY, property_data, mean)
 
-d <- aggregate(PRICE ~ PROPERTYCITY + PROPERTYZIP, property_data, mean)
-
-
-###GROUP BY - DPLYR - PIPE OPERATOR
+aggregate(PRICE ~ PROPERTYCITY + PROPERTYZIP, property_data, mean)
 
 library(dplyr)
+
+#group_by and pipe operator
 
 d <- property_data %>%
   select(PROPERTYCITY, PRICE) %>%
   group_by(PROPERTYCITY) %>%
   summarize(mean(PRICE, na.rm = TRUE))
 
+
 property_data %>%
-  select(PROPERTYCITY, PROPERTYZIP, PRICE) %>%
+  select(PROPERTYCITY, PRICE, PROPERTYZIP) %>%
   group_by(PROPERTYCITY, PROPERTYZIP) %>%
   summarize(mean(PRICE, na.rm = TRUE))
 
+
 d <- property_data %>%
-  select(PROPERTYCITY, PROPERTYZIP, PRICE) %>%
+  select(PROPERTYCITY, PRICE, PROPERTYZIP) %>%
   group_by(PROPERTYCITY, PROPERTYZIP) %>%
   summarize(Mean_Price = mean(PRICE, na.rm = TRUE))
